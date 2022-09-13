7ddf46f19a6291b605516a12c2c42486237f7deb
wasim akram <waseemjanyaro@gmail.com>
2022-09-13 07:36:54 -0700

    version

---
+++ question3.java
@@ -0,0 +1,12 @@
+public class question3 {
+    // public void sum(float [] number){
+    //     float sum=0.0f;
+    //     for(int i=0;i<number.length;i++){
+    //          sum+=i;
+    //     }
+    // }
+
+    public static void main(String[] args) {
+     
+    }
+}
